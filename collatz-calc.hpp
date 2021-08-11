d5c1007d3e582b34a67887fb9852e3c22dbd629a
PoCc001 <49920248+PoCc001@users.noreply.github.com>
2021-08-11 13:01:58 +0200

    upload libary headers

---
+++ collatz-calc.hpp
@@ -0,0 +1,45 @@
+#ifndef COLLATZ_CALC_HPP
+#define COLLATZ_CALC_HPP
+
+/**
+ * This file is part of Yet Another Collatztester.
+ *
+ *  Copyright (C) 2021  Johannes Kloimböck
+ *
+ *  Yet Another Collatztester is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Yet Another Collatztester is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Yet Another Collatztester.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+#include "carpcontainer.hpp"
+#include <intrin.h>
+
+#define trailingZerosCount(x) (_tzcnt_u64((v)))
+
+#define is_even_small(x) (!((x) & 1))
+#define is_odd_small(x) ((x) & 1)
+
+#define is_even_big(x) ((x).isEven())
+#define is_odd_big(x) ((x).isOdd())
+
+#define odd_small(x) ((x) * 3 + 1)
+#define even_small(x) ((x) >> 1)
+#define even_multiple_small(x, s) ((x) >> (s))
+#define even_all_small(x) (even_multiple_small((x), trailingZerosCount((x))))
+
+#define odd_big(x, t) ((x).mul3p1(t))
+#define even_big(x) ((x).rshift(1))
+#define even_multiple_big(x, s) ((x).rshift((s)))
+#define even_all_big(x) (even_multiple_big((x), (x).trailingZerosCount()))
+
+#endif
---
+++ tinybigint.hpp
@@ -0,0 +1,137 @@
+#ifndef TINYBIGINT_HPP
+#define TINYBIGINT_HPP
+
+/**
+ * This file is part of Yet Another Collatztester.
+ *
+ *  Copyright (C) 2021  Johannes Kloimböck
+ *
+ *  Yet Another Collatztester is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Yet Another Collatztester is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Yet Another Collatztester.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <string>
+#include <intrin.h>
+
+#define EXCEPTION_TEXT "Exceeds size of this tinybigint!"
+
+class tinybigint {
+	std::uint64_t* dvp;
+	std::size_t size;
+	std::size_t length;
+
+public:
+	tinybigint() = delete;
+	tinybigint(unsigned long long v, std::uint64_t* mem, std::size_t s = 16) {
+		if (s < 1) {
+			throw std::string("The size must be at least 1!");
+		}
+
+		dvp = mem;
+		dvp[0] = v;
+		size = s;
+		length = 1;
+	}
+
+	tinybigint(const tinybigint& tbi) = default;
+	tinybigint(tinybigint&& tmp) noexcept = default;
+	~tinybigint() {
+		dvp = nullptr;
+		size = 0;
+		length = 0;
+	}
+
+	void set(std::uint64_t v) noexcept {
+		dvp[0] = v;
+		length = 1;
+	}
+
+	void rshift(std::size_t s) noexcept {
+		std::size_t words = s >> 6;
+		std::size_t index = s & 0x3f;
+
+		std::size_t newLength = length - words;
+
+		bool subtract = (dvp[length - 1] >> index) == 0ULL;
+
+		if (subtract) {
+			--newLength;
+		}
+
+		for (std::size_t i = 0; i < newLength - 1; ++i) {
+			dvp[i] = (dvp[i + words] >> index) | (dvp[i + words + 1] << (64 - index));
+		}
+
+		dvp[newLength - 1] = dvp[newLength + words - 1] >> index;
+
+		if (subtract) {
+			dvp[newLength - 1] |= dvp[newLength + words] << (64 - index);
+		}
+
+		length = newLength;
+	}
+
+	std::size_t trailingZerosCount() const noexcept {
+		std::size_t cnt = 0;
+
+		while (dvp[cnt] == 0) {
+			++cnt;
+		}
+
+		std::uint64_t v = dvp[cnt];
+
+		cnt <<= 6;
+
+		std::size_t i = _tzcnt_u64(v);
+		cnt += i;
+
+		return cnt;
+	}
+
+	void mul3p1(std::uint64_t* tmp) {
+		std::uint8_t carry = 1;
+		for (std::size_t i = 0; i < length; ++i) {
+			dvp[i] = _umul128(dvp[i], 3ULL, tmp + i + 1);
+			carry = _addcarry_u64(carry, dvp[i], tmp[i], dvp + i);
+		}
+
+		if (length < size && (carry != 0 || tmp[length] != 0)) {
+			dvp[length] = tmp[length] + (std::uint64_t)(carry);
+			++length;
+		} else if ((carry != 0 || tmp[length] != 0) && length == size) {
+			throw std::string(EXCEPTION_TEXT);
+		}
+	}
+
+	std::uint64_t toUInt64() const noexcept {
+		return length >= 2 ? dvp[0] : dvp[0] & 0xffffffffULL;
+	}
+
+	bool isEven() const noexcept {
+		return !(dvp[0] & 1ULL);
+	}
+
+	bool isOdd() const noexcept {
+		return dvp[0] & 1ULL;
+	}
+
+	bool isOne() const noexcept {
+		return length == 1 && dvp[0] == 1ULL;
+	}
+
+	std::size_t getLength() const noexcept {
+		return length;
+	}
+};
+
+#endif
